7131a92756131979c629d0fdf6e35951d8687bb9
Abhishek Banerjee <ak@192.168.1.3>
2024-04-29 11:22:47 +0530

    Added Tailwind CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,26 +13,28 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="header">
+    <div className="flex justify-between bg-green-200 shadow-lg sm:bg-gray-200 lg:bg-pink-50">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-50" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status {onlineStatus ? "✅" : "❌"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4 text-lg">
+            Online Status {onlineStatus ? "✅" : "❌"}
+          </li>
+          <li className="px-4 text-lg">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4 text-lg">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4 text-lg">
             <Link to="/contact"> Contact Us </Link>
           </li>
-          <li>
+          <li className="px-4 text-lg">
             <Link to="/grocery"> Grocery </Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4 text-lg">Cart</li>
           <button
             className="login"
             onClick={() => {
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,34 +1,26 @@
 import { CDN_URL } from "../Utils/constants";
 
 const RestaurantCard = (props) => {
-    const {resData} = props;
-    const {
-        cloudinaryImageId,
-        name,
-        avgRating,
-        cuisines,
-        costForTwo,
-    } = resData.info;
+  const { resData } = props;
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo } =
+    resData.info;
 
-    const {
-        deliveryTime,
-    } = resData.info.sla;
+  const { deliveryTime } = resData.info.sla;
 
-    return (
-        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
-            <img
-            className="res-logo" 
-                alt="res-logo" 
-                src= {CDN_URL
-                      + cloudinaryImageId} 
-            />
-            <h3>{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating}</h4>
-            <h4>{costForTwo}</h4>
-            <h4>{deliveryTime} mins</h4>
-        </div>
-    );
+  return (
+    <div className="m-4 p-4 w-[200px] rounded-lg bg-gray-100 hover:bg-pink-100">
+      <img
+        className="rounded-lg"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3 className="font-bold py-3 text-lg">{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime} mins</h4>
+    </div>
+  );
 };
 
 export default RestaurantCard;
